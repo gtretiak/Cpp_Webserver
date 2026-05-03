4e851b694227314326a20811d8fa9c0f524e4af4
Georgii Tretiakov <gtretiak@c2r3s4.42porto.com>
2026-05-03 21:33:13 +0100

    WIP:response

--- srcs/http/HttpResponse.cpp
+++ srcs/http/HttpResponse.cpp
@@ -8,28 +8,26 @@ HttpResponse::HttpResponse() : version_("HTTP/1.1"), statusCode_(200) {}
 
 std::string	HttpResponse::toString() const {
 	std::ostringstream	ss;
-	ss << this->version_ << " " << this->statusCode_ << " "
-	<< StatusCodes::getStatus(this->statusCode_) << "\r\n"
-	ss = version + " " + ss + " ";
-	ss += this->statusText_ + "\r\n";
-       	for (std::map<std::string, std::string>::const_iterator i = this->headers_.begin(); i != this->headers_.end(); i++)
-		res << i->first << " " << i->second << "\r\n";
+	ss << this->version_ << " " << this->statusCode_ << " ";
+	ss << this->statusText_ << "\r\n";
+       	for (std::map<std::string, std::string>::iterator i = this->headers_.begin(); i != this->headers_.end(); i++)
+		ss << i->first << ": " << i->second << "\r\n";
 	ss << "\r\n";
 	ss << this->body_;
 	return (ss.str());
 }
 
-void	setVersion(const std::string &v) {
+void	HttpResponse::setVersion(const std::string &v) {
 	this->version_ = v;
 }
-void	setStatus(int code) {
+void	HttpResponse::setStatus(int code) {
 	this->statusCode_ = code;
 	this->statusText_ = StatusCodes::getStatus(code);
 }
-void	setBody(const std::string &b) {
+void	HttpResponse::setBody(const std::string &b) {
 	this->body_ = b;
 }
-void	setHeader(const std::string &k, const std::string &v) {
+void	HttpResponse::setHeader(const std::string &k, const std::string &v) {
 	this->headers_[k] = v;
 }
 
