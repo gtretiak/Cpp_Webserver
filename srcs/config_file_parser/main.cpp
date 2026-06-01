615c970bd0342a70ab0cbfd4bca39c2b831d7938
Douglas Alves <rachebartm00s@proton.me>
2026-06-01 09:40:56 +0100

    main sketch

---
+++ srcs/config_file_parser/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/29 00:37:26 by dopereir          #+#    #+#             */
+/*   Updated: 2026/05/29 00:38:43 by dopereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "globalConfig.hpp"
+
+int	main(int ac, char **av)
+{
+	//take config file was argument
+	
+	//create functions to print the values
+	// isolate them
+
+	//search for edge cases to test
+}
